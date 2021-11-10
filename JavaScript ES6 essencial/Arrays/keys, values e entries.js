5bee6df0bd975029d500fa88890b05d4c0b73d27
André Luis <andreluisklr@gmail.com>
2021-11-09 23:26:14 -0300

    métodos keys, values e entries

---
+++ JavaScript ES6 essencial/Arrays/keys, values e entries.js
@@ -0,0 +1,20 @@
+/*
+	Além de usar nos Objetos: {}, você pode usar nos arrays: [].
+	
+	Object.keys(): extrai as chaves/índices.
+	Object.values: extrai os valores das chaves/índices.
+	Object.entries: extrai as keys e os values.
+*/
+
+let nossoArray = ['Zé', 'Joana', 'Ricardo'];
+
+
+console.log( Object.keys(nossoArray) );
+
+console.log( Object.values(nossoArray) );
+
+console.log( Object.entries(nossoArray) );
+
+
+
+
