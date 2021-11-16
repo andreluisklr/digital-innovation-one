0e28fff7bee105a3dcad6b99db8ac1965b74d3c2
André Luis <andreluisklr@gmail.com>
2021-11-16 16:55:08 -0300

    fetch

---
+++ Desenvolvimento avançado com JavaScript ES6/fetch/script.js
@@ -0,0 +1,14 @@
+/*
+	Fetch.
+*/
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+	.then((response) => {
+		return response.json();	
+	})
+	.then(data => {
+		console.log(data);
+	});
+
+
+
